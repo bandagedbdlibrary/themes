76a640fd80e0a78392b0f9fe084f44a90efed91a
bandagedbdlibrary <75437848+bandagedbdlibrary@users.noreply.github.com>
2020-12-03 18:28:32 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Clear Vision</title>
+  <meta http-equiv="refresh" content="0; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ" />
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html><meta http-equiv="refresh" content="0; url=http://example.com/" />
